d3a7d2c2b62b932f037612fc9b6ee0b430a3bc8c
Derek Edwards <dedwards8@gmail.com>
2017-02-24 16:59:39 -0500

    Initial setup (#2)
    * Rename Path
    
    * Rename Path
    
    * Rename Path
    
    * Rename Path
    
    * Add reloa
    
    * Add reload

--- lib/simple_translation_engine.rb
+++ lib/simple_translation_engine.rb
@@ -26,5 +26,5 @@ module SimpleTranslationEngine
     translation = Translation.find_by(locale: locale, translation_key: key)
     return translation.nil? ? 'missing translation #{locale.name}:#{key.name}' : translation.value 
   end
-
+  
 end
