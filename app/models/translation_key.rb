bbb615b3609fd4ce44fbc5b21347a32644cd04a3
Derek Edwards <dedwards8@gmail.com>
2017-10-09 13:40:53 -0400

    Create scope to hide Refernet translations

--- app/models/translation_key.rb
+++ app/models/translation_key.rb
@@ -2,6 +2,9 @@ class TranslationKey < ActiveRecord::Base
 
   has_many :translations, :dependent => :delete_all
 
+  scope :hidden,  -> { where("name REGEXP ?", "^REFERNET_"} #Filter our REFERNET Translations
+  scope :not_hidden, -> { where.not(id: hidden) }
+
   self.primary_key = :id 
   validates :name, length: { maximum: 255 }
 
